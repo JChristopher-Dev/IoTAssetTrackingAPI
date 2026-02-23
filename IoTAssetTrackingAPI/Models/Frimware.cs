5b3d463250afad1f66e6e972b0e6935548e6d731
Joshua Christopher <107114271+JChristopher-Dev@users.noreply.github.com>
2026-02-23 15:10:40 +0200

    Firmware fields updated

--- IoTAssetTrackingAPI/Models/Frimware.cs
+++ IoTAssetTrackingAPI/Models/Frimware.cs
@@ -1,18 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IoTAssetTrackingAPI.Models
 {
     public class Firmware
     {
+        [Key]
         public int FirmwareID { get; set; }
-        public int DeviceTypeID { get; set; }
-        public string Version { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public string Description { get; set; }
 
-        // Navigation property
+        [Required]
+        public int DeviceTypeID { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Version { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [MaxLength(255)]
+        public string? Description { get; set; }
+
         [ForeignKey("DeviceTypeID")]
-        public DeviceType DeviceType { get; set; }
+        public DeviceType? DeviceType { get; set; }
     }
 }
